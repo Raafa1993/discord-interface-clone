29744d02d3026cbd5c4c84a41cc2f1d693084b59
Raafa1993 <rafaelajds@gmail.com>
2020-06-28 19:07:43 -0300

    add Channelbutton

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -4,14 +4,19 @@ import { Grid } from './styles';
 
 import ServerList from '../ServerList';
 import ServerName from '../ServerName';
+import ChanelInfo from '../ChanelInfo';
+import ChannelList from '../ChannelList';
+
 
 const Layout: React.FC = () => {
     return (
         <Grid>
             <ServerList />
             <ServerName />
+            <ChanelInfo />
+            <ChannelList />
         </Grid>
-    )
+    );
 };
 
 export default Layout;
